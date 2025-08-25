c70af6e84ec3182b6975e4429a3cd39a08e9a050
ahmedsayedsa <ahmedsayedsa@users.noreply.github.com>
2025-08-25 18:48:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -65,6 +65,13 @@ class Server {
 
         // Compression
         this.app.use(compression());
+        // منع الكاش للملفات الثابتة
+        app.use((req, res, next) => {
+        res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        res.setHeader('Pragma', 'no-cache');
+        res.setHeader('Expires', '0');
+        next();
+        });
 
         // Body parsing
         this.app.use(express.json({ limit: '10mb' }));
